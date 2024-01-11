930060a2dfee8a61d3a8b041ddf3cdcfd04a5631
pengfei.dong <mail@peng-fei.name>
2024-01-11 13:00:30 +0100

    del force_ssl for prod

--- resources/ds-update-code.sh
+++ resources/ds-update-code.sh
@@ -6,5 +6,6 @@ sed -i '/ENV.fetch("SMTP_USER")/d' config/environments/production.rb
 sed -i '/ENV.fetch("SMTP_PASS")/d' config/environments/production.rb
 sed -i '/ENV.fetch("SMTP_AUTHENTICATION")/d' config/environments/production.rb
 sed -i '/ENV.fetch("SMTP_TLS")/d' config/environments/production.rb
+sed -i '/config.force_ssl/d' config/environments/production.rb
 echo '--End delete SMTP config--'
 cat config/environments/production.rb
